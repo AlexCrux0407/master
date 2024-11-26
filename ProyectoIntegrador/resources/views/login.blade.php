2fbe6c5b08801884d4f7eb624f3c9ba4a5c5648d
JoseMiguel6814 <jowsjose6814@gmail.com>
2024-11-26 00:24:42 -0600

    nuevo diseno login/register

--- ProyectoIntegrador/resources/views/login.blade.php
+++ ProyectoIntegrador/resources/views/login.blade.php
@@ -5,19 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>  
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <style>
-        .login-image {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .login-image img {
-            max-width: 150px;
-            border-radius: 10px;
-            transition: all 0.3s ease;
-        }
-    </style>
+   
 </head>
 <body>
     <div class="login-container">
