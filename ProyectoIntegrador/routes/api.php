d7b47f38ff5acf35af2b5a0e50aa5701ed688a0b
Kripxx <125685085+Kripxx@users.noreply.github.com>
2025-05-23 11:33:59 -0600

    Funcionalidad con api

--- ProyectoIntegrador/routes/api.php
+++ ProyectoIntegrador/routes/api.php
@@ -1,16 +1,16 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginApiController;
 use App\Http\Controllers\Api\RegistroApiController;
-use App\Http\Controllers\Api\TriviaApiController;
-use App\Http\Controllers\Api\HistoriasApiController;
+//use App\Http\Controllers\Api\TriviaApiController;
+//use App\Http\Controllers\Api\HistoriasApiController;
 
 Route::post('/login', [LoginApiController::class, 'login']);
 Route::post('/register', [RegistroApiController::class, 'store']);
 
-Route::get('/trivia', [TriviaApiController::class, 'generateQuiz']);
-Route::post('/trivia/submit', [TriviaApiController::class, 'submitQuiz']);
+//Route::get('/trivia', [TriviaApiController::class, 'generateQuiz']);
+//Route::post('/trivia/submit', [TriviaApiController::class, 'submitQuiz']);
 
-Route::get('/historias', [HistoriasApiController::class, 'index']);
-Route::post('/historias', [HistoriasApiController::class, 'store']);
-Route::delete('/historias/{id}', [HistoriasApiController::class, 'destroy']);
+//Route::get('/historias', [HistoriasApiController::class, 'index']);
+//Route::post('/historias', [HistoriasApiController::class, 'store']);
+//Route::delete('/historias/{id}', [HistoriasApiController::class, 'destroy']);
